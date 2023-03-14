427f1dfda0bdf51b1743fc5f2c009fdeaed94a23
cojocaru-dan <dan.cojocaru90233@gmail.com>
2023-03-14 14:15:08 -0700

    forEach method and Callback Functions

--- corrections.js
+++ corrections.js
@@ -2,7 +2,63 @@ import {toExport as movies} from "./data.js";
 
 
 // YOUR CODE COMES HERE
+let globalGenre = "Drama";
 
+function includeGenre(movie) {
+  if (movie["genres"].includes(globalGenre)) {
+    this.push(movie["title"]);
+  }
+}
+
+// 1
+function dramas(array) {
+  globalGenre = "Drama";
+	const result = [];
+	array.forEach(includeGenre, result);
+	return result;
+}
+console.log(dramas(movies));
+
+// 2
+function findByGenre(genre, array) {
+  globalGenre = genre;
+	const newArr = [];
+	array.forEach(includeGenre, newArr);
+	return newArr;
+}
+
+console.log(findByGenre("Sci-Fi", movies));
+console.log(findByGenre("Romance", movies));
+console.log(findByGenre("Comedy", movies));
+
+// 3
+function longestMovie(array) {
+	let longestRt = array[0];
+	array.forEach(function(movie) {
+    if (movie.runtime > longestRt.runtime) {
+			longestRt = movie;
+		}
+  });
+	return longestRt;
+}
+
+console.log(longestMovie(movies));
+
+// 4
+function longestMovieByGenre(genre, array) {
+	let longestM = array[0];
+  const fctExpression = function(movie) {
+    if (movie.genres.includes(genre) && movie["runtime"] > longestM["runtime"]) {
+			longestM = movie;
+		}
+  };
+	array.forEach(fctExpression);
+	return longestM;
+}
+
+console.log(longestMovieByGenre("Romance", movies));
+console.log(longestMovieByGenre("Sci-Fi", movies));
+console.log(longestMovieByGenre("Mystery", movies));
 
 
 
